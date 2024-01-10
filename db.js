12c4af45c97ee9e0ac30466c31045958367218cc
abv4594 <abv4594@gmail.com>
2024-01-10 12:55:14 +0200

    Menu selection and improvements

---
+++ db.js
@@ -0,0 +1,47 @@
+const db =
+[
+    {
+        category: "Appliances",
+        name: "Refrigerator",
+        description: "Used for 5 years, in perfect condition, never had issues",
+        price: 1000,
+        imgUrl: "./img/img.jpg"
+    },
+    {
+        category: "Appliances",
+        name: "Tami4 BubbleX",
+        description: "Used for 4 years, in perfect condition, never had issues",
+        price: 1000,
+        imgUrl: "./img/img.jpg"
+    },
+    {
+        category: "Sports",
+        name: "Bike",
+        description: "Used for 4 years, in perfect condition, never had issues",
+        price: 1000,
+        imgUrl: "./img/img.jpg"
+    },
+    {
+        category: "Furnitures",
+        name: "Double Bed",
+        description: "Used for 4 years, in perfect condition, never had issues",
+        price: 1000,
+        imgUrl: "./img/img.jpg"
+    },
+    {
+        category: "Furnitures",
+        name: "TV Rack",
+        description: "Used for 4 years, in perfect condition, never had issues",
+        price: 1000,
+        imgUrl: "./img/img.jpg"
+    },
+    {
+        category: "Electronics",
+        name: "TV 55",
+        description: "Used for 4 years, in perfect condition, never had issues",
+        price: 1000,
+        imgUrl: "./img/img.jpg"
+    }
+            
+];
+export default db; 
--- main.js
+++ main.js
@@ -1,42 +1,40 @@
-let db = 
-        [
-                {
-                category: "Appliance",
-                name: "Refrigerator",
-                description: "Used for 5 years, in perfect condition, never had issues",
-                price: 1000,
-                imgUrl: "./img/img.jpg"
-                },
-                {
-                category: "Appliance",
-                name: "Tami4 BubbleX",
-                description: "Used for 4 years, in perfect condition, never had issues",
-                price: 1000,
-                imgUrl: "./img/img.jpg"
-                },
-                {
-                category: "Sports",
-                name: "Bike",
-                description: "Used for 4 years, in perfect condition, never had issues",
-                price: 1000,
-                imgUrl: "./img/img.jpg"
-                }      
-            
-        ];
+import db from './db.js';
         
 window.addEventListener('DOMContentLoaded', event => {
 
+    
+
     const gridContainer = document.getElementById('itemGrid');
 
-    db.forEach(item => {
+    // select menu buttons and add event listeners:
+    const buttons = document.querySelectorAll('.menu button');
+    buttons.forEach(button => {
+        button.addEventListener('click', function(e) {
+            sessionStorage.setItem("menu", e.target.textContent.toLowerCase());
+            location.reload();
+        })
+    })
+
+    // handles the menu selection
+    const filter = sessionStorage.getItem("menu");
+
+    let dbFiltered;
+
+    if (filter && filter !== 'all') {
+        dbFiltered = db.filter(item => item.category.toLocaleLowerCase() == filter)
+    } else {
+        dbFiltered = db;
+    }
+
+    dbFiltered.forEach(item => {
         const itemEl = document.createElement('div');
         itemEl.classList.add('grid-item');
         console.log(item);
         itemEl.innerHTML = `
             <img src="${item.imgUrl}" alt="${item.name}" width="250" height="250">
-            <h3>${item.name}</h3>
-            <p>${item.description}</p>
-            <p>Price: $${item.price}</p>
+            <h3 class="itemName">${item.name}</h3>
+            <p class="itemDescription">${item.description}</p>
+            <p class="itemPrice"> Price: $${item.price}</p>
         `;
         gridContainer.appendChild(itemEl);
     })
