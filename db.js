c803751ec9832948af4f33bf17c150883740f53e
abv4594 <abv4594@gmail.com>
2024-03-21 19:39:06 +0200

    Added selection and pdf download

--- db.js
+++ db.js
@@ -1,125 +1,155 @@
 const db =
 [
     {
+        id: 0,
         category: "Electronics",
         name: "Samsung Monitor 22",
         description: "Model S22F350FHM",
         price: 200,
         available: "now",
-        imgUrl: "./img/monitorSamsung.jpg"
+        imgUrl: "./img/monitorSamsung.jpg",
+        selected: false
     },
     {
+        id: 1,
         category: "Electronics",
         name: "Scanner Fujitsu",
         description: "Model IX500",
         price: 590,
         available: "now",
-        imgUrl: "./img/scannerFujitsu.jpg"
+        imgUrl: "./img/scannerFujitsu.jpg",
+        selected: false
     },
     {
+        id: 2,
         category: "Electronics",
         name: "Xbox One S",
         description: "Xbox One S with two controllers and games",
         price: 550,
         available: "now",
-        imgUrl: "./img/xBox.jpg"
+        imgUrl: "./img/xBox.jpg",
+        selected: false
     },
     {
+        id: 3,
         category: "Appliances",
         name: "Oven",
         description: "Oven Constructa pyrolytic CF4M78020Y",
         price: 990,
         available: "15-May-2024",
-        imgUrl: "./img/forno.jpg"
+        imgUrl: "./img/forno.jpg",
+        selected: false
     },
     {
+        id: 4,
         category: "Appliances",
         name: "Refrigerator",
         description: "Model Haier 547l",
         price: 990,
         available: "15-May-2024",
-        imgUrl: "./img/geladeira.jpg"
+        imgUrl: "./img/geladeira.jpg",
+        selected: false
     },
     {
+        id: 5,
         category: "Appliances",
         name: "TV 55",
         description: "TV Phillips 55",
         price: 700,
         available: "now",
-        imgUrl: "./img/tvPhillips55.jpg"
+        imgUrl: "./img/tvPhillips55.jpg",
+        selected: false
     },
     {
+        id: 6,
         category: "Appliances",
         name: "Washing machine",
         description: "Washing machine 8Kg Samsung",
         price: 700,
         available: "15-May-2024",
-        imgUrl: "./img/maqLavar.jpg"
+        imgUrl: "./img/maqLavar.jpg",
+        selected: false
     },
     {
+        id: 7,
         category: "Appliances",
         name: "Clothes dryer",
         description: "Drying machine 7Kg Indesit",
         price: 600,
         available: "15-May-2024",
-        imgUrl: "./img/maqSecar.jpg"
+        imgUrl: "./img/maqSecar.jpg",
+        selected: false
     },
  
     {
+        id: 8,
         category: "Appliances",
         name: "Dishwasher",
         description: "Dishwasher electra",
         price: 250,
         available: "15-May-2024",
-        imgUrl: "./img/lavaLouca.jpg"
+        imgUrl: "./img/lavaLouca.jpg",
+        selected: false
     },
     {
+        id: 9,
         category: "Furnitures",
         name: "Bed + Mattress",
         description: "Bed + Mattress 120 x 190 - used 2 years only",
         price: 890,
         available: "15-May-2024",
-        imgUrl: "./img/bedQueen.jpg"
+        imgUrl: "./img/bedQueen.jpg",
+        selected: false
     },
     {
+        id: 10,
         category: "Furnitures",
         name: "Study table",
         description: "Study table ikea 120x60",
         price: 250,
         available: "Now",
-        imgUrl: "./img/studyTable.jpg"
+        imgUrl: "./img/studyTable.jpg",
+        selected: false
     },
     {
+        id: 11,
         category: "Furnitures",
         name: "Mirror",
         description: "Mirror 40x150",
         price: 150,
         available: "Now",
-        imgUrl: "./img/mirror.jpg"
+        imgUrl: "./img/mirror.jpg",
+        selected: false
     },
     {
+        id: 12,
         category: "Furnitures",
         name: "Student chair",
         description: "Student chair Ikea",
         price: 180,
         available: "Now",
-        imgUrl: "./img/studyChair.jpg"
+        imgUrl: "./img/studyChair.jpg",
+        selected: false
     },
     {
+        id: 13,
         category: "Furnitures",
         name: "Office chair",
         description: "Office chair Ikea",
         price: 240,
         available: "Now",
-        imgUrl: "./img/officeChair.jpg"
+        imgUrl: "./img/officeChair.jpg",
+        selected: false
     },
     {
+        id: 14,
         category: "Furnitures",
         name: "Single double bed",
         description: "Single double bed 90x200 cm, includes two mattresses",
         price: 390,
         available: "Now",
-        imgUrl: "./img/doubleBed.jpg"
+        imgUrl: "./img/doubleBed.jpg",
+        selected: false
     }
             
 ];
--- main.js
+++ main.js
@@ -1,8 +1,52 @@
 import db from './db.js';
-        
+
+const {jsPDF} = window.jspdf;
+
+let dbFiltered;
+let dbDynamic;
+
+let generatePDF = () => {
+    const doc = new jsPDF();
+    let y = 10;
+    let pageNumber = 1;
+    let total = 0;
+    let dbSelected = dbDynamic.filter(item => item.selected);
+
+    dbSelected.forEach((product, index) => {
+        if (y > 250) { // Check if space for the next item is available on the page
+            doc.addPage(); // Add a new page
+            y = 10; // Reset y position
+            pageNumber++; // Increment page number
+        }
+        doc.text(`Name: ${product.name}`, 10, y);
+        doc.text(`Description: ${product.description}`, 10, y + 10);
+        doc.text(`Price: ${product.price}`, 10, y + 20);
+        total += product.price;
+        y += 40;
+
+        // Add page number at the bottom
+        if (index === dbFiltered.length - 1) {
+            doc.text(`Page ${pageNumber}`, 10, doc.internal.pageSize.height - 10);
+        }
+    });
+    doc.text(`Total of your products: ${total}`, 10, y);
+
+    doc.save('myProducts.pdf');
+}
+
+
 window.addEventListener('DOMContentLoaded', event => {
 
+    if (sessionStorage.getItem('db')) {
+        dbDynamic = JSON.parse(sessionStorage.getItem('db'));
+    } else {
+        dbDynamic = db;
+    }
+
     const gridContainer = document.getElementById('itemGrid');
+
+    const downloadBtn = document.getElementById('downloadBtn');
+    downloadBtn.addEventListener('click', generatePDF);
 
     // select menu buttons and add event listeners:
     const buttons = document.querySelectorAll('.menu button');
@@ -25,18 +69,21 @@ window.addEventListener('DOMContentLoaded', event => {
     // handles the menu selection
     const filter = sessionStorage.getItem("menu");
 
-    let dbFiltered;
+    
 
     if (filter && filter !== 'all') {
-        dbFiltered = db.filter(item => item.category.toLocaleLowerCase() == filter)
+        dbFiltered = dbDynamic.filter(item => item.category.toLocaleLowerCase() == filter)
     } else {
-        dbFiltered = db;
+        dbFiltered = dbDynamic;
     }
 
     dbFiltered.forEach(item => {
         const itemEl = document.createElement('div');
         itemEl.classList.add('grid-item');
-        console.log(item);
+        if (item.selected) itemEl.classList.add('selected');
+        itemEl.setAttribute('data-id', item.id);
+        itemEl.addEventListener('click', selectItem)
+        console.log(item.selected);
 
         /* handle the whatsapp link */
         const isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
@@ -61,11 +108,26 @@ window.addEventListener('DOMContentLoaded', event => {
             <p class="itemPrice"> Price: ${item.price} NIS</p>
             <p class="itemAvailability"> Available: ${item.available}</p>
             <a href=${whatsappLink} target="_blank">
-                Let's close by Whats
+                I want more info about this item
             </a>
         `;
         gridContainer.appendChild(itemEl);
     })
 
 
+
 })
+
+function selectItem(e){
+    e.target.classList.toggle('selected');
+    let selectedItem = dbDynamic.filter(item => item.id === parseInt(e.target.getAttribute('data-id')))[0];
+    if (selectedItem.selected) {
+        selectedItem.selected = false;
+    } else {
+        selectedItem.selected = true;
+    }
+    sessionStorage.setItem('db', JSON.stringify(dbDynamic)); // store status of db
+}
+
+
+
